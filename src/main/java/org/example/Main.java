e872e959456552d410f2837ddfc83ff9476c498b
aleperser4 <aleperser4@alu.edu.gva.es>
2025-10-20 18:20:37 +0200

    Principio ejercicios  UD3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuVehiculos menuVehiculos = new MenuVehiculos();
+        menuVehiculos.seleccionarOpcion();
+    }
+    }
---
+++ src/main/java/org/example/MenuVehiculos.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.Scanner;
+
+public class MenuVehiculos {
+    public static String seleccionarOpcion(int opcion){
+        Scanner lector = new Scanner(System.in)
+        System.out.println("Inserte un numero del manú: ");
+        System.out.println("1 : Mantenimiento de clientes");
+        System.out.println("2 : Introducción de facturas");
+        System.out.println("3 : Listado de facturas");
+        System.out.println("4 : Finalizar");
+
+        while(opcion){
+            case 1 : opcion= "Mantenimiento de clientes";break;
+            case 2 : opcion= "Introducción de facturas";break;
+            case 3 : opcion= "Listado de facturas";break;
+            case 4 : opcion= "Finalizar";break;
+
+        }
+        System.out.println("No existe opcion");
+
+    }
+}
